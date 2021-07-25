1bb70ffbabfa7828ef607bad2d8dad41b28948d0
allefhmedeiros <allefhmedeiros@gmail.com>
2021-07-25 11:19:37 -0300

    Criamos a classe page

--- index.php
+++ index.php
@@ -1,20 +1,19 @@
 <?php 
 
-require_once("vendor/autoload.php");
+    require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+    $app = new \Slim\Slim();
 
-$app->config('debug', true);
+    $app->config('debug', true);
 
-$app->get('/', function() {
-        
-        $sql = new Hcode\DB\Sql();
-        $results = $sql->select("SELECT * FROM tb_users");
-        echo json_encode($results);
-	//echo "OK";
+    $app->get('/', function() {
 
-});
+            $page = new Hcode\Page();
 
-$app->run();
+            $page->setTpl("index");
+
+    });
+
+    $app->run();
 
  ?>
