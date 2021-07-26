e34ddd7d8b8c934cf679cb2a6030f22cd4c12aaf
allefhmedeiros <allefhmedeiros@gmail.com>
2021-07-26 19:56:27 -0300

    Criando o pageamdin e o TPL

--- index.php
+++ index.php
@@ -3,9 +3,17 @@
     require_once("vendor/autoload.php");
     $app = new \Slim\Slim();
     $app->config('debug', true);
+    
+    //Rota de Renderização:: Site
     $app->get('/', function() {
             $page = new Hcode\Page();
             $page->setTpl("index");
     });
+    
+    //Rota de Renderização:: Administração do site
+    $app->get('/admin', function() {
+            $page = new Hcode\PageAdmin();
+            $page->setTpl("index");
+    });    
     $app->run();
  ?>
